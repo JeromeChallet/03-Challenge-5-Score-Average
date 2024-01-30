aef353edfede9653867f2255e3ca8c064b586f28
jrme421 <jeromepchallet@gmail.com>
2024-01-30 23:46:21 +0900

    solution

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-const calcAverage = (score1, score2, score3) => {
-  return score1 + score2 + score3;
-};
+/* Write your code below. Good luck! 🙂 */
+
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
 
 const scoreDolphins = calcAverage(score1, score2, score3);
 const scoreKoalas = calcAverage(score1, score2, score3);
 
-checkWinner(avgDolphins, avgKoalas);
-{
+function checkWinner(avgDolphins, avgKoalas) {
   if (avgDolphins < avgKoalas) {
-    console.log(`Koalas win (${scoreDolphins} vs. ${scoreKoalas})`);
+    console.log(`Koalas win (${scoreKoalas} vs. ${scoreDolphins})`);
   } else if (avgDolphins > avgKoalas) {
-    console.log(`Dolphins win (${scoreKoalas} vs. ${scoreDolphins})`);
+    console.log(`Dolphins win (${scoreDolphins} vs. ${scoreKoalas})`);
   } else {
     console.log("No team wins...");
   }
 }
+scoreDolphins(44, 23, 71);
+scoreKoalas(65, 54, 49);
 
-checkWinner((44, 23, 71), (85, 54, 41));
+checkWinner(scoreDolphins, scoreKoalas);
